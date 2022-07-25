62298d580d0334801596c6a6b8bd4b9881ae0a03
shanky-ced <shashankrai2022jun@cedcoss.com>
2022-07-25 13:27:34 +0530

    Functionality of TODO added

---
+++ src/Components/App.js
@@ -0,0 +1,50 @@
+
+import React,{useState} from 'react';
+import Main from './main';
+
+const App = () => {
+  const [tasks,setTasks] = useState([
+    {
+        id:1,
+        task:"Pay Bills",
+        check:true
+    },
+    {
+        id:2,
+        task:"Go Shopping",
+        check:true
+    },
+    {
+        id:3,
+        task:"See the Doctor",
+        check:false
+    }
+]);
+
+// To Delete Task
+const deleteTask = (id) =>{
+  setTasks(tasks.filter((val)=>val.id!==id))
+}
+// To Edit Task
+const editTask = (id,task) =>{
+  setTasks(tasks.map((val)=> val.id === id ? {id:val.id,task:task,check:val.check}:val))
+}
+
+// To Add Task
+const addTask = (task,check)=>{
+const id = Math.floor(Math.random()* 100)+1;
+const newTask = {id,task,check};
+setTasks([...tasks,newTask]);
+}
+// Toggle Check Uncheck
+const toggleCheck = (id) =>{
+  setTasks(tasks.map((val)=>val.id === id ? {...val,check: !val.check}:val))
+}
+
+
+  return (
+   <><Main tasks={tasks} onDelete={deleteTask} onEdit={editTask} onAdd={addTask} onToggle={toggleCheck} /></>
+  )
+}
+
+export default App;
---
+++ src/Components/main.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useState } from 'react';
+
+
+const Main = ({tasks,onDelete,onEdit,onAdd,onToggle}) => {
+    const [task, setTask ] = useState('');
+    const [check, setCheck] = useState(false);
+    const [id,setID] = useState('');
+    const submit = () =>{
+        const x = document.querySelector('.btn').innerText;
+        if(!task){
+            alert("Task input field can't be empty");
+        }
+        x === 'Update' ? onEdit(id,task) : onAdd(task,check);
+        setTask('');
+        document.querySelector('.btn').innerText = "Add";
+    }
+    const onEd = (id,text)=>{
+        setTask(text);
+        document.getElementById('new-task').value = text;
+        document.querySelector('.btn').innerText = "Update";
+        setID(id);
+    }
+    return (
+        <>
+            <div className="container">
+
+                <h2>TODO LIST</h2>
+                <h3>Add Item</h3>
+                <p>
+                    <input id="new-task" type="text" value={task}  onChange={(e)=>{setTask(e.target.value)}} /><button className="btn" onClick={submit}>Add</button>
+                </p>
+
+                <h3>Todo</h3>
+                <ul id="incomplete-tasks" >
+                    {tasks.map((val)=>{
+                        return val.check ?<></>: <><li key={val.id}><input type="checkbox" className="checkbox" checked={val.check} onClick={(e)=>{onToggle(val.id)}} /><label>{val.task}</label><input type="text" /><button
+                        className="edit" onClick={()=>{onEd(val.id,val.task)}}>Edit</button><button className="delete" onClick={()=>{onDelete(val.id)}}>Delete</button></li></>
+                    })}
+                </ul>
+
+                <h3>Completed</h3>
+                <ul id="completed-tasks">
+                {tasks.map((val)=>{
+                        return val.check ?<><li key={val.id}><input type="checkbox" className="checkbox" checked={val.check} onClick={(e)=>{onToggle(val.id)}} /><label>{val.task}</label><input type="text" /><button
+                        className="edit" onClick={()=>{onEd(val.id,val.task)}}>Edit</button><button className="delete" onClick={()=>{onDelete(val.id)}}>Delete</button></li></>:<></>
+                    })}
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Main;
